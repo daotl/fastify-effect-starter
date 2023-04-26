b9cdecf408fa91dc2a55a3859fed84d3bd81b5f2
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-04-26 21:16:03 +0800

    fix: upgrade @daotl-effect/trpc

--- _src/hello/trpc.ts
+++ _src/hello/trpc.ts
@@ -4,6 +4,7 @@ export const router = () =>
   t.router({
     hello: p.public
       .input(z.object({ username: z.string().optional() }).optional())
+      .output(z.object({ text: z.string() }))
       .query(({ input, ctx }) => {
         return Effect.succeed({
           text: `hello ${input?.username ?? ctx.user?.name ?? 'world'}`,
